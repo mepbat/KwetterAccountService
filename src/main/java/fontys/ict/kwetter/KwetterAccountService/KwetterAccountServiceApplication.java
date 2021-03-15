60bceff1a2ed700ef99ff2691b2094d6fbdfc0f4
i383656 <s.westerburger@student.fontys.nl>
2021-03-15 10:04:45 +0100

    first commit

---
+++ src/main/java/fontys/ict/kwetter/KwetterAccountService/KwetterAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package fontys.ict.kwetter.KwetterAccountService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwetterAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwetterAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fontys/ict/kwetter/KwetterAccountService/KwetterAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fontys.ict.kwetter.KwetterAccountService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwetterAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
