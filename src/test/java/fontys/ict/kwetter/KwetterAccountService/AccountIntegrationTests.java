598bc8229d86d92c4f0424730ab437a569024e81
i383656 <s.westerburger@student.fontys.nl>
2021-04-26 14:09:06 +0200

    Made Tweet and Tag ManyToMany Made Tweet and Mention ManyToMany

--- src/test/java/fontys/ict/kwetter/KwetterAccountService/AccountIntegrationTests.java
+++ src/test/java/fontys/ict/kwetter/KwetterAccountService/AccountIntegrationTests.java
@@ -1,13 +1,16 @@
 package fontys.ict.kwetter.KwetterAccountService;
 
+import fontys.ict.kwetter.KwetterAccountService.controllers.AccountController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.util.Assert;
 
 @SpringBootTest
-class KwetterAccountServiceApplicationTests {
+class AccountIntegrationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
